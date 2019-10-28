e7ca3fc0a6f0e5881e580d2aa67427cce099c7ff
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-10-29 00:02:20 +0900

    shop

--- AutoBattle_portotype/Assets/Script/L/Shop/Rarity/Dawn.cs
+++ AutoBattle_portotype/Assets/Script/L/Shop/Rarity/Dawn.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Dawn : MonoBehaviour
 {
+    string sTanker = "TANK";
+    GameObject m_Dawn;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerManager.instance.iBalance == 10)
+        {
+            Instantiate(Resources.Load("Prefabs/" + sTanker), transform.position, transform.rotation);
+        }
     }
 }
