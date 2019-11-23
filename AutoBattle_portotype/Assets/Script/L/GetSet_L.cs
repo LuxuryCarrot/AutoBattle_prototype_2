d6613d359e01f8b2b9ef0f650d97ad24a2c910b6
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-11-24 07:04:50 +0900

    chess set
    체스말 옮기는거 구현중

---
+++ AutoBattle_portotype/Assets/Script/L/GetSet_L.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSet_L : MonoBehaviour
+{
+    public Transform chess;
+    public Transform tile;
+
+    private Vector3 startPos;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000, 1024))
+            {
+                chess = hit.transform;
+                startPos = chess.position;
+
+            }
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            if (chess != null)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 1000, 6144))
+                {
+                    chess.position = hit.point + new Vector3(0, 2, 0);
+                    tile = hit.transform;
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (chess != null)
+            {
+                if (tile.gameObject.layer == 12)
+                {
+                    chess.position = startPos;
+                }
+                else if (tile != null)
+                {
+                    chess.position = tile.position + new Vector3(0, 2, 0);
+                    //chess.GetComponent<ChessFSMManager>().Settled();
+                    chess.tag = "chess";
+                    //chess.gameObject.layer = 0;
+                }
+
+            }
+            chess = null;
+            tile = null;
+        }
+    }
+}
