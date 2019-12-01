1d64ce363df1c8b9a55aea2fd765eb26a57efb75
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-12-02 01:29:50 +0900

    Update TileSet.cs

--- AutoBattle_portotype/Assets/Script/K/TileSet.cs
+++ AutoBattle_portotype/Assets/Script/K/TileSet.cs
@@ -12,25 +12,25 @@ public class TileSet : MonoBehaviour
         bool isBlack = true;
         for(int i=0; i<8; i++)
         {
-            for(int j=0; j<8; j++)
-            {
-                if (isBlack)
-                {
-                    GameObject tile = Instantiate(tiles[0], new Vector3(j*2,0,i*2), Quaternion.identity);
-                    isBlack = false;
-                }
-                else
-                {
-                    GameObject tile = Instantiate(tiles[1], new Vector3(j*2, 0, i*2), Quaternion.identity);
-                    isBlack = true;
-                }
+            //for(int j=0; j<8; j++)
+            //{
+            //    if (isBlack)
+            //    {
+            //        GameObject tile = Instantiate(tiles[0], new Vector3(j*2,0,i*2), Quaternion.identity);
+            //        isBlack = false;
+            //    }
+            //    else
+            //    {
+            //        GameObject tile = Instantiate(tiles[1], new Vector3(j*2, 0, i*2), Quaternion.identity);
+            //        isBlack = true;
+            //    }
                 
 
-            }
-            if (isBlack)
-                isBlack = false;
-            else
-                isBlack = true;
+            //}
+            //if (isBlack)
+            //    isBlack = false;
+            //else
+            //    isBlack = true;
 
             GameObject store = Instantiate(storeTile, new Vector3(i*2, 0, -4), Quaternion.identity);
         }
