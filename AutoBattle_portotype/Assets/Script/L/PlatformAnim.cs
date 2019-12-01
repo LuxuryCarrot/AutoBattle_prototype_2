43b4d9c36531c0f9e2c7489e18791f707bf6cde4
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-12-02 02:33:31 +0900

    platform time set

---
+++ AutoBattle_portotype/Assets/Script/L/PlatformAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformAnim : MonoBehaviour
+{
+    public static PlatformAnim instance;
+    public Animator anim;
+    public int iPlatformParam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlatformAnim.instance = this;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetInteger("PlatformParam", iPlatformParam);
+    }
+}
