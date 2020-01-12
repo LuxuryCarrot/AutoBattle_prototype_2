6f69544d4e6fa1200e38d78b25045307da757efb
kauthor <54879926+kauthor@users.noreply.github.com>
2020-01-13 05:50:55 +0900

    113

---
+++ AutoBattle_portotype/Assets/Script/K/NewScene/ButtonIdentity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonIdentity : MonoBehaviour
+{
+    public static string[] icons 
+        = new string[5] { "Sword", "Shield", "Whip", "Whip", "Whip" };
+    public static string[] prefabs
+        = new string[5] { "ParkWarrior", "ParkShield", "Archer", "Mage", "Ranger" };
+
+
+    private Image spriteImg;
+    private int info;
+    public ChessInitialize store;
+
+    private void Awake()
+    {
+        info = (int)Random.Range(0, 5);
+        gameObject.GetComponent<Button>().image =
+            Resources.Load("Textures/UI/CharacterIcon/" + icons[info]) as Image;
+        store = GameObject.FindGameObjectWithTag("StoreFunction").GetComponent<ChessInitialize>();
+    }
+
+    public void PurchaseButtonOn()
+    {
+        store.InitializingWithInfo(1, prefabs[info]);
+    }
+}
